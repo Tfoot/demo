ae3a4dd6e0cc98fb536a66565ad23ead972672b3
unknown <nibbisaid@gmail.com>
2021-04-25 08:25:53 +0530

    added something about while loops line

--- for_loops.cpp
+++ for_loops.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main(){
     for (int i = 0; i < 10; i++)
     {
-        cout<<i+1<<endl;    }
-    
+        cout<<i+1<<endl;  
+          }
+    cout<<"done with for loops\n";
+    cout<<"wait about while loops\n";
 
     return 0;
 }
